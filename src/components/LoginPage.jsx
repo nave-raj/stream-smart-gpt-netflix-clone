d9e66dbcbf723988e060e5761747c0351436e277
Naveena Kandasamy Rajkumar <naveraj07@gmail.com>
2024-07-20 21:12:25 -0500

    fixes for route protection

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,11 +1,12 @@
 import React, { useRef, useState } from 'react'
 import NavBar from './NavBar'
 import { validateLoginFields } from '../utils/validate';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from '../utils/firebase';
 import { addUser } from '../utils/store/userSlice';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { BACKGROUND } from '../utils/constant';
 
 
 const LoginPage = () => {
@@ -35,6 +36,13 @@ const LoginPage = () => {
                  .then((userCredential) => {
                     // Signed up 
                     const user = userCredential.user;
+                    updateProfile(auth.currentUser, {
+                        displayName: fullName.current.value,
+                    }).then(() => {
+                        navigate('/');
+                    }).catch((error) => {
+                        setErrorMessage(error.message);
+                    });
                     console.log(user);
                  }).catch((err) => {
                     const errorCode = err.code;
@@ -63,9 +71,9 @@ const LoginPage = () => {
 
     return (
         <>
-            <div className='absolute items-center justify-center'>
+            <div className='absolute h-screen items-center justify-center'>
                 <NavBar/>
-                <img src='https://assets.nflxext.com/ffe/siteui/vlv3/0552717c-9d8c-47bd-9640-4f4efa2de663/52e4cd00-9a33-4f8b-afa0-6623070f7654/US-en-20240701-POP_SIGNUP_TWO_WEEKS-perspective_WEB_6392408d-671b-40c8-83c8-888c04ea535d_medium.jpg' alt='background' className='h-screen' />
+                <img src={BACKGROUND} alt='background' className='h-screen' />
             </div>
             <div className='flex items-center justify-center h-screen relative'>
                 <form 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
-import { signOut } from "firebase/auth";
+import React, { useEffect } from 'react'
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from '../utils/firebase';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { removeUser } from '../utils/store/userSlice';
+import { LOGO } from '../utils/constant';
 
 const NavBar = () => {
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  useEffect(()=> {
+    const removeAuthStateChange = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate('/browse');
+      } else {
+        // User is signed out
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+
+    // to remove the onAuthStateChange callback
+    return () => removeAuthStateChange();
+  }, []);
+
   const handleSignOut = () => {
       signOut(auth).then(() => {
         dispatch(removeUser());
@@ -21,7 +38,7 @@ const NavBar = () => {
 
   return (
     <div className='absolute py-3 px-10 bg-gradient-to-b from-black z-10 w-screen flex justify-between'>
-        <img className='w-40' src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo" />
+        <img className='w-40' src={LOGO} alt="logo" />
       <div>
         <button  onClick={handleSignOut} className='text-white m-2 p-2 font-bold'>Sign Out</button>
       </div>
